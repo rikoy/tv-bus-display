3d964836fcc3a786cb20eb769dcb387821ac6520
Ricky Jun <djoeniar@gmail.com>
2025-03-01 14:33:26 +0700

    first

---
+++ files/list_videos.php
@@ -0,0 +1,9 @@
+<?php
+$dir = "./"; // Direktori tempat video disimpan
+$videos = array_values(array_filter(scandir($dir), function ($file) {
+    return strpos($file, ".mp4") !== false; // Ambil hanya file MP4
+}));
+
+header("Content-Type: application/json");
+echo json_encode($videos);
+?>
