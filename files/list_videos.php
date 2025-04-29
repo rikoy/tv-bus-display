ce9ea5519692c7203f8c5b44e7f048710ef8dc37
Ricky Jun <djoeniar@gmail.com>
2025-04-29 22:13:14 +0700

    update mov

--- files/list_videos.php
+++ files/list_videos.php
@@ -1,7 +1,7 @@
 <?php
 $dir = "./"; // Direktori tempat video disimpan
 $videos = array_values(array_filter(scandir($dir), function ($file) {
-    return strpos($file, ".mp4") !== false; // Ambil hanya file MP4
+    return strpos($file, ".mov") !== false; // Ambil hanya file MP4
 }));
 
 header("Content-Type: application/json");
